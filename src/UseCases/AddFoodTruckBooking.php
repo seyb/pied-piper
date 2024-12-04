db52c236e2b8cac39709053deb247a53bbb66600
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2024-12-04 12:26:06 +0100

    Implement Booking Repository for Food Truck Bookings
    Added an InMemoryBookingRepository to handle food truck booking persistence. Updated the AddFoodTruckBooking use case to integrate with the new BookingRepository interface for saving bookings. Modified tests to ensure bookings are being correctly stored using the in-memory repository.

--- src/UseCases/AddFoodTruckBooking.php
+++ src/UseCases/AddFoodTruckBooking.php
@@ -3,20 +3,25 @@
 namespace App\UseCases;
 
 use App\Domain\AddFoodTruckBooked;
+use App\Domain\BookingRepository;
 use App\Domain\FoodTruck;
+use App\Domain\FoodTruckBooking;
 use Psr\Log\LoggerInterface;
 
 class AddFoodTruckBooking
 {
     private LoggerInterface $logger;
-    public function __construct(LoggerInterface $logger)
+    private BookingRepository $bookingRepository;
+    public function __construct(BookingRepository $bookingRepository, LoggerInterface $logger)
     {
         $this->logger = $logger;
+        $this->bookingRepository = $bookingRepository;
     }
 
     public function book(FoodTruck $foodtruck, \DateTime $date): AddFoodTruckBooked
     {
         $this->logger->info('Adding a booking for {foodtruck} on {date}' , ['foodtruck', $foodtruck, 'date', $date]);
+        $this->bookingRepository->save(new FoodTruckBooking());
         return new AddFoodTruckBooked();
     }
 
--- tests/AddFoodTruckBookingTest.php
+++ tests/AddFoodTruckBookingTest.php
@@ -3,21 +3,50 @@
 namespace App\Tests;
 
 use App\Domain\AddFoodTruckBooked;
+use App\Domain\BookingRepository;
 use App\Domain\FoodTruck;
+use App\Domain\FoodTruckBooking;
 use App\UseCases\AddFoodTruckBooking;
+
+class InMemoryBookingRepository implements BookingRepository
+{
+    public bool $isSaved;
+
+    public function __construct()
+    {
+        $this->isSaved = false;
+    }
+
+    public function save(FoodTruckBooking $booking): bool
+    {
+        $this->isSaved = true;
+        return $this->isSaved;
+    }
+}
 
 class AddFoodTruckBookingTest extends ApplicationTestCase
 {
-    function test_calling_adding_a_booking_succeeds()
+    function test_it_succeeds_adding_a_booking()
     {
-        $logger = $this->initializeLoggerMock();
-        $useCase = new AddFoodTruckBooking($logger);
+        $useCase = new AddFoodTruckBooking(new InMemoryBookingRepository(), $this->initializeLoggerMock());
         $foodtruck = new FoodTruck();
         $date = new \DateTime();
 
-        $this->assertEquals(new AddFoodTruckBooked(), $useCase->book($foodtruck, $date));
+        $result = $useCase->book($foodtruck, $date);
+
+        $this->assertEquals(new AddFoodTruckBooked(), $result);
     }
 
 
+    function test_it_stores_the_booking()
+    {
+        $bookingRepository = new InMemoryBookingRepository();
+        $useCase = new AddFoodTruckBooking($bookingRepository, $this->initializeLoggerMock());
+        $foodtruck = new FoodTruck();
+        $date = new \DateTime();
 
+        $useCase->book($foodtruck, $date);
+
+        $this->assertTrue($bookingRepository->isSaved);
+    }
 }
