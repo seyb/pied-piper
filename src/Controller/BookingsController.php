ca8aaed30373c8f44bda2c71483c5f3f602f8f26
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2024-12-06 14:23:21 +0100

    Return HTTP 204 for empty bookings list.
    Updated the BookingsController to return a 204 No Content response when no bookings are available, reflecting the absence of data more accurately. Adjusted the BookingsControllerTest to assert the HTTP status code 204, ensuring the behavior is tested and verified.

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookingsController extends AbstractController
@@ -11,9 +12,7 @@ class BookingsController extends AbstractController
     #[Route('/api/bookings', name: 'app_bookings')]
     public function index(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookingsController.php',
-        ]);
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
+
--- tests/BookingsControllerTest.php
+++ tests/BookingsControllerTest.php
@@ -3,14 +3,16 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookingsControllerTest extends WebTestCase
 {
-    public function test_list_bookings(): void
+    public function test_it_returns_empty_list_bookings_when_no_data(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/api/bookings');
+        $client->request('GET', '/api/bookings');
 
         $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_NO_CONTENT);
     }
 }
